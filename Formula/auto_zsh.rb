ea14138b07aab9d319bb83d169ba80e595d0a56d
Aleksander Dushku <aleksanderdushku@hotmail.com>
2024-12-06 13:30:36 +0000

    added new configs

--- Formula/auto_zsh.rb
+++ Formula/auto_zsh.rb
@@ -1,14 +1,13 @@
 class AutoZsh < Formula
   desc "Automates Zsh setup with Oh My Zsh, plugins, and themes"
   homepage "https://github.com/kuqja/homebrew-auto_zsh"
-  url "https://github.com/kuqja/homebrew-auto_zsh/archive/refs/heads/auto_sh.zip"
+  url "https://github.com/kuqja/homebrew-auto_zsh/blob/d79fef90003e9dfb996dad845156f5215a55777b/auto_sh.zip"
   sha256 "3d10163ba5340f3d598d675b1f7d7b2229f0c0afc56035973592eb85365125c1"
   version "1.0.0"
 
   def install
     bin.install "auto_zsh.sh"
   end
-
   def caveats
     <<~EOS
       To start Zsh setup, run:
