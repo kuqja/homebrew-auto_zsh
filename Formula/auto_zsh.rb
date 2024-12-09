e4bb33d435ae7294081d25b095124ebae060f16f
Aleksander Dushku <aleksanderdushku@hotmail.com>
2024-12-09 19:33:24 +0000

    adding new sha

--- Formula/auto_zsh.rb
+++ Formula/auto_zsh.rb
@@ -2,7 +2,7 @@ class AutoZsh < Formula
   desc "Automates Zsh setup with Oh My Zsh, plugins, and themes"
   homepage "https://github.com/kuqja/homebrew-auto_zsh"
   url "https://github.com/kuqja/homebrew-auto_zsh/blob/main/auto_zsh.zip"
-  sha256 "d3ac2c1b33d143d59e898b60c0470b82014442d04002327847faba7a9d83f58b"
+  sha256 "f020cc4fa2fccca7a85b38e0911718659bb302fbda1b396815fc906175233fe2"
   version "1.0.0"
 
   def install
